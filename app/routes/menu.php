77ce56c8b233136918a2f53f34c5511ce2310779
ponbiki <jcandroscavage@hotmail.com>
2015-11-09 16:39:31 -0500

    zone list in session

--- app/routes/menu.php
+++ app/routes/menu.php
@@ -4,6 +4,8 @@ use ponbiki\apiCheat as cheat;
 
 $app->get('/menu', function() use ($app) {
     
+    echo "<pre style='color:white'>";print_r($_SESSION['zones']);echo "</pre>";
+    
     if (!isset($_SESSION['api_key'])) {
         $app->redirect('/');
     }
@@ -16,7 +18,8 @@ $app->get('/menu', function() use ($app) {
         'meta' => $meta,
         'info' => $_SESSION['info'],
         'error' => $_SESSION['error'],
-        'loggedin' => $_SESSION['loggedin']
+        'loggedin' => $_SESSION['loggedin'],
+        'zones' => $_SESSION['zones']
     ]);
     
     cheat\Session::clear();    
