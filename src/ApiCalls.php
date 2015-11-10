c757fc2dd27b540723532b6666c1f157c19c392c
ponbiki <jcandroscavage@hotmail.com>
2015-11-09 20:04:53 -0500

    separated out protected method baseCurl in ApiCalls Class

--- src/ApiCalls.php
+++ src/ApiCalls.php
@@ -4,15 +4,21 @@ namespace ponbiki\apiCheat;
 
 class ApiCalls implements iApiCalls
 {
-    public function keyValidate($key)
+    protected function baseCurl($key, $arg)
     {
         $clean_key = \filter_var($key, FILTER_SANITIZE_STRING);
         $ch = \curl_init();
-        \curl_setopt($ch, CURLOPT_URL, self::BASEURL ."zones");
+        \curl_setopt($ch, CURLOPT_URL, self::BASEURL . $arg);
         \curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-NSONE-Key: $clean_key"));
         \curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $body = \json_decode(\curl_exec($ch), true);
         \curl_close($ch);
+        return $body;
+    }
+    public function keyValidate($key)
+    {
+        $arg = "zones";
+        $body = self::baseCurl($key, $arg);
         if (array_key_exists('message', $body)) {
             return \FALSE;
         } else {
